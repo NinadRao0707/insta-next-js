92f795aebbbf76fd3e44d6b7c492bd3d26735cfe
Ninad Rao <67018142+NinadRao0707@users.noreply.github.com>
2021-12-13 18:21:54 +0530

    Created Insta Header

---
+++ components/Feed.js
@@ -0,0 +1,15 @@
+function Feed() {
+    return (
+        <div>
+            {/* Section */}
+                {/* Stories */}
+                {/* Post */}
+
+            {/* Section */}
+                {/* Mini Profile */}
+                {/* Suggestions */}
+        </div>
+    )
+}
+
+export default Feed
