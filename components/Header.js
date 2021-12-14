b384b1f80a71699e9c593aec6a48bd782cfb3851
Ninad Rao <67018142+NinadRao0707@users.noreply.github.com>
2021-12-14 18:48:31 +0530

    Final insta page ready

--- components/Header.js
+++ components/Header.js
@@ -7,13 +7,16 @@ import {
     PaperAirplaneIcon,
     MenuIcon,
 } from '@heroicons/react/outline'
-import {HomeIcon} from '@heroicons/react/solid'
+import { HomeIcon } from '@heroicons/react/solid'
 import { signIn, signOut, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router';
+import { useRecoilState } from 'recoil';
+import { modalState } from '../atoms/ModalAtom';
 
 function Header() {
     const { data: session } = useSession();
     const router = useRouter();
+    const [open, setOpen] = useRecoilState(modalState);
     return (
         <div className="shadow-sm border-b bg-white sticky top-0 z-50">
             <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
@@ -45,7 +48,7 @@ function Header() {
                                 <PaperAirplaneIcon className="navBtn rotate-45" />
                                 <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
                             </div>
-                            <PlusCircleIcon className="navBtn" />
+                            <PlusCircleIcon onClick={() => setOpen(true)} className="navBtn" />
                             <UserGroupIcon className="navBtn" />
                             <HeartIcon className="navBtn" />
                             <img onClick={signOut} src={session?.user?.image} alt="Profile Pic" className="h-10 rounded-full cursor-pointer" />
--- components/Posts.js
+++ components/Posts.js
@@ -1,34 +1,18 @@
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../firebase';
 import Post from './Post';
 
-const posts = [
-    {
-        id: 1,
-        username: 'Ninad',
-        userImg: 'https://pbs.twimg.com/profile_images/1302896410510069761/pyAcmGHK_400x400.jpg',
-        img: 'https://pbs.twimg.com/profile_images/1302896410510069761/pyAcmGHK_400x400.jpg',
-        caption: 'My first post !!',
-    },
-    {
-        id: 2,
-        username: 'Ninad',
-        userImg: 'https://pbs.twimg.com/profile_images/1302896410510069761/pyAcmGHK_400x400.jpg',
-        img: 'https://pbs.twimg.com/profile_images/1302896410510069761/pyAcmGHK_400x400.jpg',
-        caption: 'My first post !!',
-    },
-    {
-        id: 3,
-        username: 'Ninad',
-        userImg: 'https://pbs.twimg.com/profile_images/1302896410510069761/pyAcmGHK_400x400.jpg',
-        img: 'https://pbs.twimg.com/profile_images/1302896410510069761/pyAcmGHK_400x400.jpg',
-        caption: 'My first post !!',
-    },
-]
-
 function Posts() {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => 
+        onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), snapshot => {
+            setPosts(snapshot.docs);
+        }), [db])
     return (
         <div>
             {posts.map(post => (
-                <Post key={post.id} id={post.id} username={post.username} userImg={post.userImg} img={post.img} caption={post.caption}/>
+                <Post key={post.id} id={post.id} username={post.data().username} userImg={post.data().profileImg} img={post.data().image} caption={post.data().caption}/>
             ))}
         </div>
     )
